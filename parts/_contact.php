6ff7effc637bdaa3f3fd307a36064fdde1d36556
Kristopher Watts <Kristopher.A.Watts@gmail.com>
2015-05-27 21:38:54 +0000

    Small Contact fix

--- parts/_contact.php
+++ parts/_contact.php
@@ -1,19 +1,14 @@
 <?php
-
-function contact(  ) {
-    /* if ( isset( $_REQUEST['submit'] ) ) { */
-        $message = $_REQUEST['message'];
-        $from = $_REQUEST['name'];
-        $fromMail = $_REQUEST['email'];
-        $to = "Kristopher.A.Watts@gmail.com";
-        $mailSubject = "Message from Webtastic-Development.net";
-        $headers = "From: $from <$fromMail>";
-        $subject = "$mailSubject";
-        $toEmail = "$to";
-        $message = "$fromMail\n" . $message;
-        mail( $toEmail, $subject, $message, $headers );
-        header( $_SERVER[ 'SERVERNAME' ] );
-    /* } */
-}
+    $message = $_REQUEST['message'];
+    $from = $_REQUEST['name'];
+    $fromMail = $_REQUEST['email'];
+    $to = "Kristopher.A.Watts@gmail.com";
+    $mailSubject = "Message from Webtastic-Development.net";
+    $headers = "From: $from <$fromMail>";
+    $subject = "$mailSubject";
+    $toEmail = "$to";
+    $message = "$fromMail\n" . "$from\n" . $message;
+    mail( $toEmail, $subject, $message, $headers );
+    header( "Location: http://www.webtastic-development.net" );
 
 ?>
