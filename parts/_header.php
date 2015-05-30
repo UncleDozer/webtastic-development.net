3202222a4aa98c73132b8f810e94a7d144622a8c
Kristopher Watts <Kristopher.A.Watts@gmail.com>
2015-05-30 18:18:08 +0000

    Temporarily removed about link

--- parts/_header.php
+++ parts/_header.php
@@ -14,7 +14,7 @@
         <ul class="header--nav--list">
             <li class="header--nav--list--item"><a href="?location=home">Home</a></li>
             <li class="header--nav--list--item"><a href="?location=projects">Projects</a></li>
-            <li class="header--nav--list--item"><a href="?location=about">About Me</a></li>
+            <!--<li class="header--nav--list--item"><a href="?location=about">About Me</a></li>  --!>
             <li class="header--nav--list--item"><a href="https://Github.com/UncleDozer">My Github</a></li>
         </ul>
     </nav>
