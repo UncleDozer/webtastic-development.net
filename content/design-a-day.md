8635fc106b10fc049993a685f2154653a8adc37c
Kristopher Watts <Kristopher.A.Watts@gmail.com>
2015-07-01 23:04:11 +0000

    Design a day

---
+++ content/design-a-day.md
@@ -0,0 +1,4 @@
+# Design A Day
+To keep my frontend skills sharp, I've decided to code a new design every day. Sometimes these will be designs I've found ( on dribbble.com or elsewhere ) in which case I will give full credit to the author. This week is a custom designed card, inspired by Google's new Material Design. 
+
+* #[July 01, 2015 ]( http://webtastic-development.net/design-a-day/july-01 )
--- parts/_header.php
+++ parts/_header.php
@@ -14,6 +14,7 @@
         <ul class="header--nav--list">
             <li class="header--nav--list--item"><a href="?location=home">Home</a></li>
             <li class="header--nav--list--item"><a href="?location=projects">Projects</a></li>
+            <li class="header--nav--list--item"><a href="?location=design-a-day">Design A Day</a></li>
             <!--<li class="header--nav--list--item"><a href="?location=about">About Me</a></li>  --!>
             <li class="header--nav--list--item"><a href="https://Github.com/UncleDozer">My Github</a></li>
         </ul>
